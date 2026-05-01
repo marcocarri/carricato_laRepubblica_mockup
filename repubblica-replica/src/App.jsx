cb3be63c844deed148af1c10acb858a1fb7f462c
marcocarri <carricatomarco@gmail.com>
2026-05-01 15:35:07 +0200

    pulizia codice, correzione indentazione e aggiunta dei commenti per App.jsx, main.jsx e index.css

--- repubblica-replica/src/App.jsx
+++ repubblica-replica/src/App.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import MainBody from './components/MainBody'; // Aggiungi questo import
+import MainBody from './components/MainBody';
 
 function App() {
     return (
         <div className="app-wrapper min-vh-100 d-flex flex-column bg-white">
+
+            {/* --- intestazione --- */}
             <Header />
 
-            {/* Corpo Principale della pagina */}
+            {/* --- area dei contenuti --- */}
             <div className="flex-grow-1">
                 <MainBody />
             </div>
 
+            {/* --- pié di pagina --- */}
             <Footer />
+
         </div>
     );
 }
